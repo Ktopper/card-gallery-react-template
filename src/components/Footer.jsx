02f7b3ce7c44bb64efa7fee5fa50720d14423674
Ktop <ktopdunham@gmail.com>
2023-08-11 13:48:12 -0400

    added new stuff

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,18 +1,37 @@
+import { useEffect, useState } from "react";
+import "../css/footer.css";
+import MailButton from "./MailButton";
 
-import { Link } from 'react-router-dom';
+function Footer() {
+  const [showFooter, setShowFooter] = useState(false);
 
-function Footer(){
- 
+  useEffect(() => {
+    setTimeout(() => {
+      setShowFooter(true);
+    }, 1000);
+  }, []);
 
-
-return(
-  <div className="footer-area">
-    <ul>
-
-      <li><Link to="/contact">Contact</Link></li>
-    </ul>
-  </div>
-
-)}
+  return (
+    <div className={`footer-area ${showFooter ? "show" : ""}`}>
+      <div className="footer-buttons">
+        <a href="link of your choice">
+          <img
+            className="air-bnb-button"
+            src="../images/home/air_bnb_logo.png"
+            alt="air bnb logo"
+          ></img>
+        </a>
+        <MailButton
+          recipient={"Your name@.com"}
+          subject={"Subject of Email"}
+        />
+      </div>
+      <div className="footer-text">
+        <p>© 2023 -name of biz-.<br/> All Rights Reserved.</p>
+        <p> Website by -your biz- LLC. <br/>yourName@gmail.com</p>
+      </div>
+    </div>
+  );
+}
 
 export default Footer;
